943fe3fb5387ca100efbe85b782d3b55388216de
deepak-ranolia <d.ranolia92@gmail.com>
2018-08-22 13:10:58 +0530

    scroll to top

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Inject, HostListener } from '@angular/core';
+import { DOCUMENT } from "@angular/platform-browser";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  navIsFixed: boolean
+
+  constructor( @Inject(DOCUMENT) private document: Document ){ }
+  @HostListener("window:scroll", [])
+  onWindowScroll(){
+    if ( window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop > 100) {
+      this.navIsFixed = true
+    } else if ( this.navIsFixed && window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop < 10 ){
+      this.navIsFixed = false
+    }
+  }
+  scrollToTop(){
+    (function smoothscroll() {
+      var currentScroll = document.documentElement.scrollTop || document.body.scrollTop; if (currentScroll > 0) {
+        window.requestAnimationFrame(smoothscroll)
+        window.scrollTo(0, currentScroll - (currentScroll / 5))
+      }
+    })()
+  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, HostListener } from '@angular/core';
 import { ProjectService } from '../services/ProjectService';
+// declare var $: any;
+import { DOCUMENT } from "@angular/platform-browser";
 
 @Component({
   selector: 'app-home',
@@ -7,12 +9,13 @@ import { ProjectService } from '../services/ProjectService';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  navIsFixed: boolean
 
-  options: any;
-  section6: any;
-  section7: any;
+  options: any
+  section6: any
+  section7: any
 
-  constructor(private projectService: ProjectService) {
+  constructor( private projectService : ProjectService ){
 
     // this.projectService.emitDontShowSection6.subscribe(res=>{
     //   this.section6 = res.show;
@@ -23,11 +26,12 @@ export class HomeComponent implements OnInit {
     // });
 
   }
-
+  
   ngOnInit() {
-    this.checkLogin();
+    this.checkLogin()
   }
 
+  
   checkLogin() {
     let temp = localStorage.getItem('token');
     if(temp === "true") {
@@ -36,5 +40,9 @@ export class HomeComponent implements OnInit {
       this.projectService.logout();
     }
   }
+  // upArrow(){
+  //   $("html, body").animate({ scrollTop: 0 }, "slow")
+  //   return false  
+  // }
 
 }
