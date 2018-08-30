f8131e08c39c833dbd0ef2d007ea0d1b8ae79922
deepak-ranolia <d.ranolia92@gmail.com>
2018-08-30 19:20:30 +0530

    dist local

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,37 +7,34 @@ import './script.js';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
+
 export class HomeComponent implements OnInit {
-  
-  options: any
-  section6: any
-  section7: any
+  options : any
+  section6 : any
+  section7 : any
+  constructor(private projectService:ProjectService) {
 
-  constructor( private projectService : ProjectService ){
-
-    // this.projectService.emitDontShowSection6.subscribe(res=>{
+    // this.projectService.emitDontShowSection6.subscribe( res => {
     //   this.section6 = res.show;
     // });
     //
-    // this.projectService.emitDontShowSection7.subscribe(res=>{
+    // this.projectService.emitDontShowSection7.subscribe( res => {
     //   this.section7 = res.show;
     // });
-
   }
 
   ngOnInit() {
     this.checkLogin()
   }
 
-
   checkLogin() {
     let temp = localStorage.getItem('token');
-    if(temp === "true") {
-
+    if( temp === "true" ){
     } else {
       // this.projectService.logout();
     }
   }
+
   // upArrow(){
   //   $("html, body").animate({ scrollTop: 0 }, "slow")
   //   return false
--- src/app/services/APIService.ts
+++ src/app/services/APIService.ts
@@ -5,8 +5,8 @@ import { EventEmitter, Injectable } from '@angular/core';
 export class APIService {
 
   projectURL: string = '../assets/APIData/';
-  fileUploadURL: string = 'http://192.168.15.187:8080/';
-  afterFileUploadURL: string = 'http://192.168.15.187:8080/';
+  fileUploadURL: string = 'http://0.0.0.0:8080/';
+  afterFileUploadURL: string = 'http://0.0.0.0:8080/';
   // projectURL: string = 'http://192.168.15.187:8000';
   // projectURL: string = 'https://qcitech.org:8081';
 
